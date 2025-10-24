c0c8632d9fcf8521b56aad8023de2e76a48696b8
kingsmancode <mathecf@hotmail.com>
2025-10-24 13:14:16 -0300

    adicionar gitignore

--- CalculadoraDepreciacao/obj/Debug/net9.0/CalculadoraDepreciacao.AssemblyInfo.cs
+++ CalculadoraDepreciacao/obj/Debug/net9.0/CalculadoraDepreciacao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculadoraDepreciacao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388d77559969f9cc58208460dbfa0dccb781f495")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculadoraDepreciacao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculadoraDepreciacao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
